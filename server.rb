bbe23278cd9003e51935f1e27c7b0d9becc44332
Helen Hood <helen.c.hood@gmail.com>
2014-08-20 14:13:29 -0400

    Initial commit

---
+++ server.rb
@@ -0,0 +1,5 @@
+# Create pages for:
+# - Page that lists all the artists & links to artist's page
+# - Page for each artist.  Has name, genre, and then name of each of their songs
+# - Page that lists all songs
+# - Page for each song, showing name of song & artist name
